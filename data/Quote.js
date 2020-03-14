01dd437d61b5a4dc0896300545424358876868fc
Andreas Hammer <andreas.hammer.97@protonmail.com>
2020-03-15 00:01:09 +0100

    V1 backend

---
+++ data/Quote.js
@@ -0,0 +1,75 @@
+import { ObjectID } from 'mongodb';
+import { getDB } from './db';
+import { getPerson } from './Person';
+
+// TODO: search by person for quote (said_by), search for quotes containing string, heartQuote, getQuotesWithSort
+
+export default class Quote {
+    constructor({ _id, text, date, votes, said_by, tags}) {
+        this.id = _id;
+        this.text = text;
+        this.date = date;
+        this.votes = votes || 0;
+        this.said_by = said_by;
+        this.tags = tags;
+    }
+}
+
+export const getQuote = id => new Promise((resolve, reject) => {
+    getDB().then(db => {
+        const collection = db.collection('quotes');
+        const query = {
+            _id: ObjectID(id)
+        };
+        const cursor = collection.find(query);
+        cursor.toArray().then(result => {
+            if (result.length == 0) {
+                return resolve(null);
+            }
+            const quote = new Quote(result[0]);
+            resolve(quote);
+        }).catch(reject);
+    }).catch(console.error);
+});
+
+export const getQuotes = (amount, page) => new Promise((resolve, reject) => {
+    getDB().then(db => {
+        const collection = db.collection('quotes');
+        collection.find({}).skip(amount * page).limit(amount).toArray().then(result => {
+            resolve(result.map(qt => new Quote(qt)));
+        }).catch(reject);
+    }).catch(reject);
+});
+
+export const addQuote = (text, date, said_by, tags) => new Promise((resolve, reject) => {
+    getDB().then(db => {
+        const persons = [said_by];
+        if (tags) {
+            tags.forEach(p => persons.push(p));
+        }
+        Promise.all(persons.map(p => getPerson(p))).then(res => {
+            if (res.filter(p => p == null).length != 0) {
+                reject('Invalid person in input');
+            }
+            else {
+                const collection = db.collection('quotes');
+                collection.insertOne({
+                    text,
+                    date,
+                    said_by,
+                    tags: tags || [],
+                    votes: 0,
+                    date: date || new Date().toUTCString(),
+                }).then(result => resolve(new Quote(result.ops[0]))).catch(reject);
+            }
+        }).catch(reject);
+    }).catch(reject);
+});
+
+export const upVote = id => new Promise((resolve, reject) => {
+    getDB().then(db => {
+        const collection = db.collection('quotes');
+        collection;
+        resolve();
+    }).catch(reject);
+});
---
+++ data/index.js
@@ -0,0 +1,20 @@
+import { getQuote, getQuotes, addQuote } from './Quote';
+import { getPerson, getPersons, addPerson } from './Person';
+
+export default () => {
+    return {
+        Query: {
+            getQuote: (parent, { id }) => getQuote(id),
+            getQuotes: (parent, { amount, page }) => getQuotes(amount, page),
+            getPerson: (parent, { id }) => getPerson(id),
+            getPersons: parent => getPersons(),
+        },
+        Mutation: {
+            addQuote: (parent, { input }) => addQuote(input.text, input.date, input.said_by, input.tags),
+        },
+        Quote: {
+            said_by: parent => getPerson(parent.said_by),
+            tags: parent => Promise.all(parent.tags.map(person => getPerson(person))),
+        },
+    };
+};
