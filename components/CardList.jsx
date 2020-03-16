3f79f661bcdc2db25a04c383b06d1bdb3a2539a2
Ole Kristian Aune <okaune@outlook.com>
2020-03-16 15:53:12 +0100

    Added infinite scroll

--- components/CardList.jsx
+++ components/CardList.jsx
@@ -1,37 +1,65 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import styled from "styled-components";
+import InfiniteScroll from "react-infinite-scroller";
+import debounce from "lodash.debounce";
 
 import { query } from "../lib/api-lib";
 
 import Card from "./Card";
 
+const CardListWrapper = styled.div`
+  max-width: 500px;
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+  margin-top: 64px;
+`;
+
+const Loader = styled.div`
+  padding: 1rem;
+  margin: 0 auto;
+  text-align: center;
+`;
+
 const CardList = () => {
+  const [error, setError] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
   const [quotes, setQuotes] = useState([]);
-  useEffect(() => {
+
+  const loadQuotes = debounce(page => {
     query(
-      `query { quotes(amount: 100, page: 0) { text, said_by{name}, date } }`
-    ).then(res => setQuotes(res?.data?.quotes.reverse()));
-  }, []);
+      `query { quotes(amount: 10, page: ${page}) { text, said_by{name}, date } }`
+    )
+      .then(res => {
+        const data = res?.data?.quotes;
+        if (data?.length < 10) setHasMore(false);
+        setQuotes([...quotes, ...res?.data?.quotes]);
+      })
+      .catch(error => {
+        console.error("test", error);
+        setError(true);
+      });
+  }, 100);
 
   return (
-    <div className="card-list">
-      {quotes.map((entry, index) => (
-        <Card
-          key={index}
-          text={entry?.text}
-          said_by={entry?.said_by?.name}
-          date={entry?.date}
-        />
-      ))}
-      <style jsx>{`
-        .card-list {
-          max-width: 500px;
-          display: flex;
-          flex-direction: column-reverse;
-          margin: 0 auto;
-          margin-top: 64px;
-        }
-      `}</style>
-    </div>
+    <InfiniteScroll
+      pageStart={-1}
+      loadMore={loadQuotes}
+      hasMore={hasMore}
+      loader={<Loader>Laster ...</Loader>}
+    >
+      <CardListWrapper>
+        {quotes.map((entry, index) => (
+          <Card
+            style={{ zIndex: quotes.length - index }}
+            key={index}
+            text={entry?.text}
+            said_by={entry?.said_by?.name}
+            date={entry?.date}
+          />
+        ))}
+      </CardListWrapper>
+    </InfiniteScroll>
   );
 };
 
