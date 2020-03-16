778be212fe2a729d0e75cdc92cd89caa4047cca6
Andreas Hammer <andreas.hammer.97@protonmail.com>
2020-03-16 02:05:53 +0100

    User API, API access restrictions and HeartQuote support

--- data/index.js
+++ data/index.js
@@ -1,18 +1,57 @@
-import { getQuote, getQuotes, addQuote } from "./Quote";
+import { getQuote, getQuotes, addQuote, heartQuote } from "./Quote";
 import { getPerson, getPersons, addPerson } from "./Person";
+import { login, register, logout } from "./Auth";
 
 export default () => {
   return {
     Query: {
-      quote: (parent, { id }) => getQuote(id),
-      quotes: (parent, { input, sort, amount, page }) =>
-        getQuotes(input, sort, amount, page),
-      person: (parent, { id }) => getPerson(id),
-      persons: (parent, { name }) => getPersons(name)
+      quote: (parent, { id }, context) => {
+        if (!context?.userSession) {
+          return Promise.reject("No user session found");
+        }
+        return getQuote(id);
+      },
+      quotes: (parent, { input, sort, amount, page }, context) => {
+        if (!context?.userSession) {
+          return Promise.reject("No user session found");
+        }
+        return getQuotes(input, sort, amount, page);
+      },
+      person: (parent, { id }, context) => {
+        if (!context?.userSession) {
+          return Promise.reject("No user session found");
+        }
+        return getPerson(id);
+      },
+      persons: (parent, { name }, context) => {
+        if (!context?.userSession) {
+          return Promise.reject("No user session found");
+        }
+        return getPersons(name);
+      }
     },
     Mutation: {
-      addQuote: (parent, { input }) =>
-        addQuote(input.text, input.date, input.said_by, input.tags)
+      addQuote: (parent, { input }, context) => {
+        if (!context?.userSession) {
+          return Promise.reject("No user session found");
+        }
+        return addQuote(input.text, input.date, input.said_by, input.tags);
+      },
+      heartQuote: (parent, { id }, context) => {
+        if (!context?.userSession) {
+          return Promise.reject("No user session found");
+        }
+        return heartQuote(id, context.userSession);
+      },
+      login: (parent, { username, password }) => login(username, password),
+      register: (parent, { username, password }) =>
+        register(username, password),
+      logout: (parent, args, context) => {
+        if (!context?.userSession) {
+          return Promise.resolve(true);
+        }
+        return logout(context.userSession);
+      }
     },
     Quote: {
       said_by: parent => getPerson(parent.said_by),
