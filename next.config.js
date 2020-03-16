5fda003029a84aab45bb34992b6c5d4ba6480e73
Andreas Hammer <andreas.hammer.97@protonmail.com>
2020-03-16 17:35:33 +0100

    Config quth required update

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     DB_NAME: process.env.DB_NAME,
     DB_USER: process.env.DB_USER,
     DB_PASSWORD: process.env.DB_PASSWORD,
-    AUTH_REQUIRED: true
+    AUTH_REQUIRED: false
   },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.module.rules.push({
