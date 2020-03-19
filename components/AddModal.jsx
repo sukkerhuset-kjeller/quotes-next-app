e0053701b7f64e829c0efd41972a71e58471d937
Ole Kristian Aune <okaune@outlook.com>
2020-03-19 01:28:18 +0100

    Moved to modal component

--- components/AddModal.jsx
+++ components/AddModal.jsx
@@ -1,34 +1,50 @@
 import { useState, useEffect } from 'react';
 import Select from 'react-select';
 import styled from 'styled-components';
+import ReactModal from 'react-modal';
 import { query } from '../lib/api-lib';
 import Button from './Button';
 
+function ReactModalAdapter({ className, modalClassName, ...props }) {
+    return (
+        <ReactModal
+            className={modalClassName}
+            portalClassName={className}
+            {...props}
+        />
+    );
+}
+
+const StyledModal = styled(ReactModalAdapter).attrs({
+    overlayClassName: 'Overlay',
+    modalClassName: 'Modal',
+})`
+    .Modal {
+        background: #ffffff;
+        border-radius: 40px;
+        width: calc(100% - 2rem);
+        max-width: 500px;
+        min-height: 100px;
+        margin: 1rem;
+        padding: 2rem;
+    }
+    .Overlay {
+        display: flex;
+        z-index: 6000000;
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        justify-content: center;
+        align-items: center;
+        background: #24133285;
+        backdrop-filter: blur(9px);
+    }
+`;
+ReactModal.setAppElement('#__next');
+
 const customSelectStyles = {};
-
-const ModalBackground = styled.div`
-    display: ${(props) => (props.show ? 'flex' : 'none')};
-    z-index: 6000000;
-    position: fixed;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    justify-content: center;
-    align-items: center;
-    background: #24133285;
-    backdrop-filter: blur(9px);
-`;
-
-const Modal = styled.div`
-    background: #ffffff;
-    border-radius: 40px;
-    width: calc(100% - 2rem);
-    max-width: 500px;
-    min-height: 100px;
-    margin: 1rem;
-    padding: 2rem;
-`;
 
 const TextField = styled.input`
     border: none;
@@ -63,43 +79,38 @@ const AddModal = ({ show, setShow, quotes, setQuotes }) => {
     }, []);
 
     return (
-        <ModalBackground show={show}>
-            <Modal>
-                <TextField
-                    type="text"
-                    placeholder="Sitat"
-                    onChange={(e) => setQuote(e.target.value)}
-                />
-                <Select
-                    instanceId="saidBy"
-                    placeholder="Sagt av"
-                    isClearable={true}
-                    isSearchable={true}
-                    styles={customSelectStyles}
-                    onChange={(value, _) => setSaidBy(value.value)}
-                    options={persons.map((person) => ({
-                        label: person.name,
-                        value: person.id,
-                    }))}
-                />
-                <AddModalButton
-                    onClick={() =>
-                        query(
-                            `mutation { addQuote(input: { text: "${quote}", saidBy: "${saidBy}", date: "${new Date().getTime()}" }) { text, saidBy{name}, date } }`
-                        ).then((res) => {
-                            if (res?.data?.addQuote) {
-                                setQuotes([
-                                    { ...res.data.addQuote },
-                                    ...quotes,
-                                ]);
-                            }
-                            setShow(false);
-                        })
-                    }>
-                    Lagre
-                </AddModalButton>
-            </Modal>
-        </ModalBackground>
+        <StyledModal isOpen={show} onRequestClose={() => setShow(false)}>
+            <TextField
+                type="text"
+                placeholder="Sitat"
+                onChange={(e) => setQuote(e.target.value)}
+            />
+            <Select
+                instanceId="saidBy"
+                placeholder="Sagt av"
+                isClearable={true}
+                isSearchable={true}
+                styles={customSelectStyles}
+                onChange={(value, _) => setSaidBy(value.value)}
+                options={persons.map((person) => ({
+                    label: person.name,
+                    value: person.id,
+                }))}
+            />
+            <AddModalButton
+                onClick={() =>
+                    query(
+                        `mutation { addQuote(input: { text: "${quote}", saidBy: "${saidBy}", date: "${new Date().getTime()}" }) { text, saidBy{name}, date } }`
+                    ).then((res) => {
+                        if (res?.data?.addQuote) {
+                            setQuotes([{ ...res.data.addQuote }, ...quotes]);
+                        }
+                        setShow(false);
+                    })
+                }>
+                Lagre
+            </AddModalButton>
+        </StyledModal>
     );
 };
 
