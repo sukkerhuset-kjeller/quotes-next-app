aeb6cdef578e496a1869d03cbd6d985fecfcba73
Andreas Hammer <andreas.hammer.97@protonmail.com>
2020-04-03 15:19:32 +0200

    Revert "Coldfix/auth cookie use"

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -15,22 +15,9 @@ const resolvers = api();
 const apolloServer = new ApolloServer({
     typeDefs,
     resolvers,
-    context: async ({ req }) => {
-        const cookie = req?.headers?.cookie;
-        let sessionId = req.headers.authentication;
-        if (cookie) {
-            const cookieSuggestions = cookie
-                .split(';')
-                .map((cookie) => cookie.split('='))
-                .filter((cookie) => cookie[0] === 'session_id');
-            if (cookieSuggestions.length > 0) {
-                sessionId = sessionId[0][1];
-            }
-        }
-        return {
-            userSession: await getUserSession(sessionId),
-        };
-    },
+    context: async ({ req }) => ({
+        userSession: await getUserSession(req.headers.authentication),
+    }),
 });
 
 const handler = apolloServer.createHandler({ path: '/api/graphql' });
